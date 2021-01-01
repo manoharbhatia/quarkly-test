5490f8ff242f102468ed181762e807bd3f5ccb4f
manoharbhatia <67742034+manoharbhatia@users.noreply.github.com>
2021-01-01 20:34:50 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,31 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fef38e8a6aceb001f321c84",
+				"5fef38e8a6aceb001f321c86",
+				"5fef39b1f4d20b0022057397"
+			]
+		},
+		"5fef38e8a6aceb001f321c84": {
+			"id": "5fef38e8a6aceb001f321c84",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fef38e8a6aceb001f321c86": {
+			"id": "5fef38e8a6aceb001f321c86",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5fef39b1f4d20b0022057397": {
+			"id": "5fef39b1f4d20b0022057397",
+			"pageUrl": "sample",
+			"name": "sample"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
